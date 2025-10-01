4724749738e2b5cab8087b90f8f02a9afbdd4460
Hyerin Park <hyerinpark@Hyerinui-MacBookAir.local>
2025-10-01 18:15:20 +0900

    first

---
+++ study/251001/hello.ts
@@ -0,0 +1,22 @@
+function greet(name: string): string {
+  return `Hello, ${name}`
+}
+
+console.log(greet('Jhon'))
+
+// typescript 기본 자료형
+// 숫자
+let age: number = 20
+age = 20.5
+// age = "20"
+
+const PI: number = 3.14
+
+// 문자열
+let nickname: string = "John"
+nickname = "Doe"
+
+// let name = "Test"
+
+// 불리언
+let isActive: boolean = true
