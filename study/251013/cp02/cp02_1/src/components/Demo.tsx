253407d1c8570b82d68c71b291385ba72ee50349
Hyerin Park <hyerinpark@Hyerinui-MacBookAir.local>
2025-10-13 15:06:46 +0900

    251013 exam

---
+++ study/251013/cp02/cp02_1/src/components/Demo.tsx
@@ -0,0 +1,28 @@
+import * as D from "../data";
+
+// export default function Demo() {
+const Demo = () => {
+  const children = D.makeArray(10).map((_, index) => (
+    <div key={index}>
+      <p>{D.randomId()}</p>
+      <p>{D.randomName()}</p>
+      <p>{D.randomJobTitle()}</p>
+      <p>{D.randomSentence()}</p>
+      <img src={D.randomAvatar()} width={100} height={100} alt="avatar" />
+    </div>
+  ));
+
+  return (
+    <>
+      <h1>Demo</h1>
+      <p>
+        {D.randomName()}, {D.randomJobTitle()}, {D.randomCompanyName()}
+      </p>
+      <img src={D.randomAvatar()} width={100} height={100} alt="avatar" />
+      <img src={D.randomImage()} width={100} height={100} alt="random-image" />
+      {children}
+    </>
+  );
+};
+
+export default Demo;
