9e7a831e2cc13fd015d6cfe59bcf009f4f017a68
Hyerin Park <hyerinpark@Hyerinui-MacBookAir.local>
2025-11-04 09:54:25 +0900

    251104 useMemo, useCallback

---
+++ study/251104/vite_exam/src/pages/ExamplePage.tsx
@@ -0,0 +1,12 @@
+import UseCallbackExample from "../components/UseCallbackExample";
+import UseMemoExample from "../components/UseMemoExample";
+
+export default function ExamplePage() {
+  return (
+    <div>
+      <h1>ExamplePage</h1>
+      <UseMemoExample />
+      <UseCallbackExample />
+    </div>
+  );
+}
